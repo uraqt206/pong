3e25f272c31e8c156e0c0480a0b20734de805f17
uraqt206 <lequangtrung100306@gmail.com>
2023-07-25 15:09:12 +0700

    sv

--- my pong/Paddle.lua
+++ my pong/Paddle.lua
@@ -1,8 +1,13 @@
 
 
-Paddle = Class {
-}
+Paddle = Class {}
 
-function Paddle:init(x, y, width, height)
-  
+function Paddle:init(_x, _y, _w, _h)
+  self.x, self.y, self.width, self.height, self.dy = _x, _y, _w, _h, 0
 end
+
+function Paddle:render()
+  -- love.graphics.printf(tostring(self.w), 0, VIRTUE_HEIGHT, VIRUTE_WIDTH, 'center')
+  love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+  -- love.graphics.rectangle('line', 0, 0, 10, 20)
+end 
--- my pong/main.lua
+++ my pong/main.lua
@@ -2,21 +2,69 @@ push = require 'push'
 Class = require 'class'
 require 'Paddle'
 
+state = 'start'
+servingPlayer = 1;
+PADDLE_SPEED = 100
 
 function love.load() 
   VIRTUE_WIDTH, VIRTUE_HEIGHT = 432, 243
-  WINDOW_WIDTH, WINDOW_HEIGHT = love.window.getDesktopDimensions()
+  WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
   love.graphics.setDefaultFilter('nearest', 'nearest')
   push:setupScreen(VIRTUE_WIDTH, VIRTUE_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     fullscreen = false,
     resizable = true
   })
+  startFont = love.graphics.newFont('font.ttf', 20)
+  startFont2 = love.graphics.newFont('font.ttf', 10)
+  Paddle1 = Paddle(0, 0, 10, 30)
+  Paddle2 = Paddle(VIRTUE_WIDTH-10, VIRTUE_HEIGHT-30, 10, 30)
+end
 
-  gameFont = love.graphics.newFont('font.ttf', 20)
+function love.keypressed(key) 
+  love.graphics.printf(tostring(key), 0, 0, VIRTUE_WIDTH, 'center')
+  if key == 'return' then
+    if (state == 'serving') then
+      state = 'playing'
+    end
+
+    if state == 'start' then
+      state = 'serving'
+    end
+
+  end
+
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+function StartGame()
+  love.graphics.setFont(startFont)
+  love.graphics.printf('Welcome to the pong!', 0, VIRTUE_HEIGHT/2-6, VIRTUE_WIDTH, 'center')
+  love.graphics.setFont(startFont2)
+  love.graphics.printf('Press enter to start the game', 0, VIRTUE_HEIGHT/2+15, VIRTUE_WIDTH, 'center')
+end
+
+function Serving()
+  love.graphics.printf('Player ' .. servingPlayer .. ' to serve', 0, VIRTUE_HEIGHT/2-6, VIRTUE_WIDTH, 'center')
+  love.graphics.printf('Please press enter', 0, VIRTUE_HEIGHT/2+10, VIRTUE_WIDTH, 'center')
 end
 
 function love.draw()
   push:apply('start');
   
+  if state ~= 'start' then
+    Paddle1:render()
+    Paddle2:render()
+  end
+
+  if state == 'start' then
+    StartGame()
+  end
+  
+  if state == 'serving' then
+    Serving()
+  end
+
   push:apply('end');
 end
